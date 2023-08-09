ef02d477fa95a189db8cc6a1260d0fc6be5f6508
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-09 15:33:25 -0400

    map function added to Tours

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
-const Tours = ({tours}) => {
+const Tours = ({ tours }) => {
   return (
-   <section>
-    <h2>our Tours</h2>
-    <div className="underline">
-    </div>
-    <div>
-    {tours.map((tour) => (
-        <ul key={tour.id}>
-          <li>{tour.name}</li>
-          <li>{tour.img}</li>
-          <li>{tour.description}</li>
-        </ul>
-      ))}
-    </div>
-   </section>
+    <section>
+      <h2>our tours </h2>
+      <div className="underline"></div>
+      <div>
+        {tours.map((tour) => (
+          <ul key={tour.id}>
+            <li>{tour.name}</li>
+            <li>{tour.img}</li>
+            <li>{tour.description}</li>
+          </ul>
+        ))}
+      </div>
+    </section>
+  );
 };
