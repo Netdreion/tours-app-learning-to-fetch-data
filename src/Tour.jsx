15e8c10998611fccfec874136ed0db4dc74c5742
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-10 14:54:12 -0400

    readmore button added

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <article>
       <img src={image} alt={name} />
@@ -10,7 +11,10 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>readmore</button>{" "}
+        </p>
         <button className="delete-btn">not interested</button>
       </footer>
     </article>
