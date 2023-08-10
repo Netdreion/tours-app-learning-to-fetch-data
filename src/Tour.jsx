83910ecc9784651b70e4b620201ad61df2c9cd22
Netdreion <128006926+Netdreion@users.noreply.github.com>
2023-08-10 14:47:02 -0400

    Tour component set-up

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const Tour = () => {
+const Tour = ({ id, image, info, price, name }) => {
   return (
-    <div>
-      <h3>Tour</h3>
-    </div>
+    <article>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
   );
 };
 export default Tour;
